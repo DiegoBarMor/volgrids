7f5b1d7862e55dd6f806fb41a0d0b32295333ff9
Diego Barquero Morera <diego.barquero-morera@phys.ens.fr>
2025-07-10 17:47:16 +0200

    [WIP] testing a new score method (examples/pocket_scores.py)

--- examples/pocket_scores.py
+++ examples/pocket_scores.py
@@ -44,33 +44,40 @@ class PocketScoreCalculator:
 
 
     # --------------------------------------------------------------------------
-    def run(self, folder_data: Path):
-        for path_cmap in folder_data.glob("*.cmap"):
-            name_pdb = path_cmap.stem
+    def run(self, folder_ps: Path, folder_wh: Path) -> None:
+        for path_ps in folder_ps.glob("*.cmap"):
+            name_pdb = path_ps.stem
             print(f"Processing {name_pdb}...")
+
+            path_wh = folder_wh / f"{name_pdb}.cmap"
 
             self.data_pdb.append(name_pdb)
 
             ### boolean grid, points in space that are part of the pocket are "True"
-            pocket = vg.GridIO.read_cmap(path_cmap, f"{name_pdb}.trimming.3.0").grid.astype(bool)
+            pocket = vg.GridIO.read_cmap(path_ps, f"{name_pdb}.trimming").grid.astype(bool)
 
-            keys = set(vg.GridIO.get_cmap_keys(path_cmap))
+            keys = set(vg.GridIO.get_cmap_keys(path_ps))
             for key in keys:
                 if key.startswith(name_pdb + ".trimming"): continue
 
-                smif = vg.GridIO.read_cmap(path_cmap, key).grid
+                smif_ps = vg.GridIO.read_cmap(path_ps, key).grid
+                smif_wh = vg.GridIO.read_cmap(path_wh, key).grid
                 kind = key.split('.')[-1]
 
                 if kind == "apbs":
-                    neg = smif.copy()
-                    pos = smif.copy()
-                    neg[neg > 0] = 0
-                    pos[pos < 0] = 0
-                    self._assign_score("apbs-neg", neg, pocket)
-                    self._assign_score("apbs-pos", pos, pocket)
+                    neg_ps = smif_ps.copy()
+                    pos_ps = smif_ps.copy()
+                    neg_wh = smif_wh.copy()
+                    pos_wh = smif_wh.copy()
+                    neg_ps[neg_ps > 0] = 0
+                    pos_ps[pos_ps < 0] = 0
+                    neg_wh[neg_wh > 0] = 0
+                    pos_wh[pos_wh < 0] = 0
+                    self._assign_score("apbs-neg", neg_ps, neg_wh, pocket)
+                    self._assign_score("apbs-pos", pos_ps, pos_wh, pocket)
 
                 else:
-                    self._assign_score(kind, smif, pocket)
+                    self._assign_score(kind, smif_ps, smif_wh, pocket)
 
 
     # --------------------------------------------------------------------------
@@ -83,22 +90,26 @@ class PocketScoreCalculator:
 
 
     # --------------------------------------------------------------------------
-    def _assign_score(self, smif_kind: str, smif: vg.Grid, pocket: vg.Grid) -> None:
-        volume = len(pocket[pocket])    # the volume is given by the number of points in the pocket
-        sum_smif = np.sum(smif[pocket]) # the sum of the smif values in the pocket
+    def _assign_score(self, smif_kind: str, smif_ps: vg.Grid, smif_wh: vg.Grid, pocket: vg.Grid) -> None:
+        volume = len(pocket[pocket]) # the volume is given by the number of points in the pocket
+        sum_ps = np.sum(smif_ps)     # the sum of the smif values in the pocket
+        sum_wh = np.sum(smif_wh)     # the sum of the smif values in the whole grid
         if volume == 0:
             raise ValueError("Warning: Pocket is empty, no points in the pocket.")
 
 
         ##### Option 0: SMIF integral without normalization
-        score = np.abs(sum_smif)
+        # score = np.abs(sum_ps)
 
         ##### Option 1: SMIF integral is only normalized by the volume of the pocket
-        # score = np.abs(sum_smif) / volume
+        # score = np.abs(sum_ps) / volume
 
         ##### Option 2: SMIF integral is normalized by its maximum absolute value and by the volume of the pocket
         # maximum_abs = np.max(np.abs(smif))
-        # score = np.abs(sum_smif) / (maximum_abs * volume) if maximum_abs > 0 else 0
+        # score = np.abs(sum_ps) / (maximum_abs * volume) if maximum_abs > 0 else 0
+
+        ##### Option 3: SMIF integral of the pocket normalized by the integral of the whole grid
+        score = np.abs(sum_ps) / np.abs(sum_wh) if np.abs(sum_wh) > 0 else 0
 
 
         self.data_scores[smif_kind].append(score)
@@ -109,11 +120,14 @@ if __name__ == "__main__":
     # Run tests/smiffer/pocket_sphere.py before running this script
     # Run this script from the root folder of the repository
 
-    FOLDER_DATA  = Path("testdata/smiffer/pocket_sphere")
+    FOLDER_DATA  = Path("testdata/smiffer")
     PATH_CSV_OUT = Path("scores.csv")
 
+    FOLDER_PS    = FOLDER_DATA / "pocket_sphere"
+    FOLDER_WH    = FOLDER_DATA / "whole"
+
     psc = PocketScoreCalculator()
-    psc.run(FOLDER_DATA)
+    psc.run(FOLDER_PS, FOLDER_WH)
     psc.save(PATH_CSV_OUT)
 
     visualize_scores(PATH_CSV_OUT)
