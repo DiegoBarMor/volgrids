927ead4ad0713e5f1cac9bf3b1a5fc7df0b8c869
Diego Barquero Morera <diego.barquero-morera@phys.ens.fr>
2026-01-13 13:24:31 +0100

    added package to pypi

--- LICENSE.md
+++ LICENSE.md
@@ -1,7 +1,7 @@
 
 The MIT License (MIT)
 
-Copyright (c) 2025 Diego Barquero Morera
+Copyright (c) 2026 Diego Barquero Morera
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="volgrids",
     version="0.1.0",
     description="Framework for volumetric calculations, with emphasis in biological molecular systems.",
-    keywords="grid",
+    keywords="grid mif smif volumetric molecular structural biology interaction field",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author="DiegoBarMor",
@@ -12,9 +12,7 @@ setup(
     url="https://github.com/diegobarmor/volgrids",
     license="MIT",
     packages=find_packages(),
-    package_data={
-        "volgrids": ["_tables/*"],
-    },
+    package_data={"volgrids": ["_tables/*"]},
     install_requires=[],
     classifiers=[
         "Programming Language :: Python :: 3",
