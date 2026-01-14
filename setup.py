c704719ce4a6657da66e19fa2a9dab53f0f87ef6
Diego Barquero Morera <diego.barquero-morera@phys.ens.fr>
2026-01-14 16:24:55 +0100

    requirements now automatically installed (pip volgrids)
    updated environment requirements

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+def read_requirements(path = "requirements.txt"):
+    p = Path(path)
+    if not p.exists(): return []
+    stripped_lines = (ln.strip() for ln in p.read_text().strip().splitlines())
+    return [ln for ln in stripped_lines if ln and not ln.startswith("#")]
 
 setup(
     name="volgrids",
@@ -13,7 +20,7 @@ setup(
     license="MIT",
     packages=find_packages(),
     package_data={"volgrids": ["_tables/*", "utils/*"]},
-    install_requires=[],
+    install_requires=read_requirements(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
